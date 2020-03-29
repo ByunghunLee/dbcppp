2729450446cf29a6aaa71fe027e2974614c04650
Byunghun Lee <byunghun.lee@magna.com>
2020-03-28 23:02:50 -0400

    adding functional

--- include/dbcppp/AttributeDefinition.h
+++ include/dbcppp/AttributeDefinition.h
@@ -4,6 +4,7 @@
 #include <cstddef>
 #include <string>
 #include <vector>
+#include <functional>
 #include <boost/variant.hpp>
 
 #include "Export.h"
@@ -45,12 +46,12 @@ namespace dbcppp
             std::vector<std::string> values;
         };
         using value_type_t = boost::variant<ValueTypeInt, ValueTypeHex, ValueTypeFloat, ValueTypeString, ValueTypeEnum>;
-        
+
         static std::unique_ptr<AttributeDefinition> create(
-              std::string&& name
-            , ObjectType object_type
-            , value_type_t&& value_type);
-            
+                std::string&& name
+                , ObjectType object_type
+                , value_type_t&& value_type);
+
         virtual std::unique_ptr<AttributeDefinition> clone() const = 0;
 
         virtual ~AttributeDefinition() = default;
